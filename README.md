a4fb1f01614111aac94e7e33aca7158cd7e3c4d2
Taichiro-S <hokkaido_mx_1_t.s@me.com>
2024-01-30 14:49:00 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# image_loading_sample
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/imageloadingsample/image_loading_sample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.imageloadingsample.image_loading_sample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
